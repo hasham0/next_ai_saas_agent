ef98dbc5d4de42103aa5d87c5fce4b167f8a7b39
hasham0 <hashamsaleem75@gmail.com>
2025-06-24 20:10:21 +0500

    feat: Project setup add core UI components and utility hooks with Shadcn setup

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    rules: {
+      "no-console": "off",
+      "no-unused-vars": ["off", { argsIgnorePattern: "^_" }],
+      "prefer-const": "error",
+      eqeqeq: ["error", "always"],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
+];
+
+export default eslintConfig;
