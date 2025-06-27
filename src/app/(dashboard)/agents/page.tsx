8cda8c6a737483c5574c1738932e8864b07be8c5
hasham0 <hashamsaleem75@gmail.com>
2025-06-27 19:29:41 +0500

    feat: integrate nuqs for agent management; add search filters, pagination, and enhance data handling

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import type { SearchParams } from "nuqs";
 import { ErrorBoundary } from "react-error-boundary";
 import { auth } from "@/lib/auth";
 import AgentsListHeader from "@/modules/agents/ui/components/agents-list-header";
@@ -10,9 +11,15 @@ import {
   AgentsView,
   AgentsViewLoading,
 } from "@/modules/agents/ui/views/agents-view";
+import loadSearchParams from "@/modules/agents/utils/params";
 import { getQueryClient, trpc } from "@/trpc/server";
 
-export default async function AgentsPage() {
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
+
+export default async function AgentsPage({ searchParams }: Props) {
+  const filters = await loadSearchParams(searchParams);
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -21,7 +28,11 @@ export default async function AgentsPage() {
     redirect("/sign-in");
   }
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  void queryClient.prefetchQuery(
+    trpc.agents.getMany.queryOptions({
+      ...filters,
+    })
+  );
   return (
     <>
       <AgentsListHeader />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { NuqsAdapter } from "nuqs/adapters/next";
 import { Toaster } from "@/components/ui/sonner";
 import { TRPCReactProvider } from "@/trpc/client";
 
@@ -19,13 +20,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <TRPCReactProvider>
-      <html lang="en">
-        <body className={`${inter.className} antialiased`}>
-          {children}
-          <Toaster />
-        </body>
-      </html>
-    </TRPCReactProvider>
+    <NuqsAdapter>
+      <TRPCReactProvider>
+        <html lang="en">
+          <body className={`${inter.className} antialiased`}>
+            {children}
+            <Toaster />
+          </body>
+        </html>
+      </TRPCReactProvider>
+    </NuqsAdapter>
   );
 }
